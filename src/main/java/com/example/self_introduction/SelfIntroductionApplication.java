da82b5da2a963227a879ceaa81652698acf2df1b
tomohiro <tomohiro0123456789@gmail.com>
2022-08-16 17:39:09 +0900

    first commit

---
+++ src/main/java/com/example/self_introduction/SelfIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.self_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/self_introduction/SelfIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.self_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
